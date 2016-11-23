e089676a69aef832c877f56886f25356f877be0a
david <davidporos92@gmail.com>
2016-11-23 17:33:51 +0100

    Change function param types from Exception to Throwable

--- classes/Kohana/Kohana/Exception.php
+++ classes/Kohana/Kohana/Exception.php
@@ -78,10 +78,10 @@ class Kohana_Kohana_Exception extends Exception {
 	 * exception, and the stack trace of the error.
 	 *
 	 * @uses    Kohana_Exception::response
-	 * @param   Exception  $e
+	 * @param   Throwable  $e
 	 * @return  void
 	 */
-	public static function handler(Exception $e)
+	public static function handler(Throwable $e)
 	{
 		$response = Kohana_Exception::_handler($e);
 
@@ -96,10 +96,10 @@ class Kohana_Kohana_Exception extends Exception {
 	 * for display.
 	 *
 	 * @uses    Kohana_Exception::response
-	 * @param   Exception  $e
+	 * @param   Throwable  $e
 	 * @return  Response
 	 */
-	public static function _handler(Exception $e)
+	public static function _handler(Throwable $e)
 	{
 		try
 		{
@@ -133,11 +133,11 @@ class Kohana_Kohana_Exception extends Exception {
 	 * Logs an exception.
 	 *
 	 * @uses    Kohana_Exception::text
-	 * @param   Exception  $e
+	 * @param   Throwable  $e
 	 * @param   int        $level
 	 * @return  void
 	 */
-	public static function log(Exception $e, $level = Log::EMERGENCY)
+	public static function log(Throwable $e, $level = Log::EMERGENCY)
 	{
 		if (is_object(Kohana::$log))
 		{
@@ -157,10 +157,10 @@ class Kohana_Kohana_Exception extends Exception {
 	 *
 	 * Error [ Code ]: Message ~ File [ Line ]
 	 *
-	 * @param   Exception  $e
+	 * @param   Throwable  $e
 	 * @return  string
 	 */
-	public static function text(Exception $e)
+	public static function text(Throwable $e)
 	{
 		return sprintf('%s [ %s ]: %s ~ %s [ %d ]',
 			get_class($e), $e->getCode(), strip_tags($e->getMessage()), Debug::path($e->getFile()), $e->getLine());
@@ -170,10 +170,10 @@ class Kohana_Kohana_Exception extends Exception {
 	 * Get a Response object representing the exception
 	 *
 	 * @uses    Kohana_Exception::text
-	 * @param   Exception  $e
+	 * @param   Throwable  $e
 	 * @return  Response
 	 */
-	public static function response(Exception $e)
+	public static function response(Throwable $e)
 	{
 		try
 		{
